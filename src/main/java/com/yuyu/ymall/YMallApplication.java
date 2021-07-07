c5d9e9b1e730bc2f47b22954ebe2e4c3e66f8d3b
yuyu <ylgbd@gmail.com>
2021-07-07 12:42:49 +0800

    first commit

---
+++ src/main/java/com/yuyu/ymall/YMallApplication.java
@@ -0,0 +1,13 @@
+package com.yuyu.ymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuyu/ymall/YMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyu.ymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
